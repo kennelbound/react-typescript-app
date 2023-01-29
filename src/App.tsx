22fb0e46cc6b7971bbd523455a1412ac5dbbdc07
Kennelbound <github-primary@kennelbound.com>
2023-01-29 13:05:30 -0600

    Adding support for bootstrap components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import {Button, Card} from "react-bootstrap";
 
 const App = () => {
-    return         <div className="App">
-        This is your app here
-    </div>;
+    return <Card >
+        <Card.Img variant="top" src="logo512.png"/>
+        <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+    </Card>
 }
 export default App;
